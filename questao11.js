db554f59243deeac550a0b3f979fe63a96f6564c
GabrielAtlas <joaogabrielcosta13@icloud.com>
2022-06-14 14:13:23 -0300

    final commit

---
+++ questao11.js
@@ -0,0 +1,7 @@
+function multiplicacao(num1, num2) {
+  let resultado = num1 * num2;
+  return resultado;
+}
+multiplica(4, 7); // multiplica is not defined
+multiplica(5, 6); // multiplica is not defined
+multiplica(3, 3); // multiplica is not defined
